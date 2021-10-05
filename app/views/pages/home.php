d20d323beb0cf310d0714d677e83f34c1779a5c8
unknown <murdzek.mikolaj@gmail.com>
2021-10-05 19:27:16 +0200

    Changes

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -1,84 +1,19 @@
 <?php $this->view("layout/header", $data) ?>
 <div class="container pt-4 mb-4">
-    <div class="row">
+    <div class="row text-center pt-5">
         <div class="col-md-12">
             <div class="jumbotron">
                 <h2>
-                    Hello, world!
+                    Twój własny notes!
                 </h2>
                 <p>
-                    This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+                    Aplkacja pozwala na zapisywanie własnych notatek. Możesz je dodawać, edytować i usuwać.
                 </p>
-                <p>
-                    <a class="btn btn-primary btn-large" href="#">Learn more</a>
-                </p>
-            </div>
-            <dl>
-                <dt>
-                    Description lists
-                </dt>
-                <dd>
-                    A description list is perfect for defining terms.
-                </dd>
-                <dt>
-                    Euismod
-                </dt>
-                <dd>
-                    Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-                </dd>
-                <dd>
-                    Donec id elit non mi porta gravida at eget metus.
-                </dd>
-                <dt>
-                    Malesuada porta
-                </dt>
-                <dd>
-                    Etiam porta sem malesuada magna mollis euismod.
-                </dd>
-                <dt>
-                    Felis euismod semper eget lacinia
-                </dt>
-                <dd>
-                    Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                </dd>
+
             </dl>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-4">
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-    </div>
+
 </div>
 
 <!---->
